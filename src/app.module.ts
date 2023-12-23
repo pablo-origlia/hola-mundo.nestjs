183d5d6fe772bc6dc40e2d8eea8858997e9801ec
Pablo Origlia <85880626+pablo-origlia@users.noreply.github.com>
2023-12-22 21:20:45 -0300

    Parámetros en las rutas de aplicaciones NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsController } from './products/products.controller';
+import { UsersController } from './controllers/users/users.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ProductsController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
